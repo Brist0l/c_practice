8ba13931ce65c96eb8b0ecf0f245281c3c030090
mrHola21 <gautamthegreat21@gmail.com>
2021-10-27 14:51:46 +0530

    added lots of stuff

---
+++ The_C_programming_Lang/1-5.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+// orignal code
+
+/*int main(){
+
+	int fahr;
+
+	for (fahr = 0;fahr<=300;fahr = fahr+20){
+
+		printf("%3d %6.1f\n",fahr,(5.0/9.0)*(fahr-32));
+	}
+}*/ 
+
+// mine
+int main(){
+
+	int fahr;
+
+	for (fahr = 300;fahr<=0;fahr = fahr -20){
+		printf("%3d %6.1f\n",fahr,(5.0/9.0)*(fahr-32));
+	}
+}
---
+++ The_C_programming_Lang/symbolic.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+#define LOWER 	0
+
+int main(){
+
+	printf(LOWER);
+	return 0;
+}
