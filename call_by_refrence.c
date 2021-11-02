ac5284d9c0c4cc33dfbfd33d4856171cbf8e4738
mrHola21 <gautamthegreat21@gmail.com>
2021-11-02 13:29:08 +0530

    Added pointers 6

--- call_by_refrence.c
+++ call_by_refrence.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 
-void increment(int a){
-	a = a+1;
-	printf("Address of a in increment is  %d\n",&a);
+void increment(int *p){
+	*p = *p + 1;
+	printf("Address of a in increment is  %d\n",p);
 
 }
 
 int main(){
 	int a;
 	a = 10;
-	increment(a);
+	increment(&a);
 	printf("The Address of a in main is %d\n",&a);
-
+	printf("%d",a);
 
 }
